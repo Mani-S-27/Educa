0278677052f42ef51d6a61222b1b3402c16f0f94
Mani-S-27 <m2026maet006@stud.tiss.ac.in>
2026-09-03 17:03:00 +0530

    Add files via upload

---
+++ MAET 2026.mm
@@ -0,0 +1,244 @@
+<map version="freeplane 1.12.15">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks/>
+<node TEXT="MAET 2026" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1788341304496" BACKGROUND_COLOR="#f0f0f0" STYLE="bubble" CHILD_NODES_LAYOUT="AUTO_FLOW">
+<font SIZE="22"/>
+<hook NAME="MapStyle" zoom="0.513158">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" auto_compact_layout="true" show_tags="UNDER_NODES" associatedTemplateLocation="template:/standard-1.6.mm" show_note_icons="true" fit_to_viewport="true" show_icons="BESIDE_NODES" showTagCategories="false" newNodeStyle=":style:defaultstyle.floating"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="20" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Sem 1" POSITION="bottom_or_right" ID="ID_1833662509" CREATED="1788333425094" MODIFIED="1788340648703" STYLE="fork" TEXT_WRITING_DIRECTION="LEFT_TO_RIGHT">
+<font SIZE="22"/>
+<edge COLOR="#ff0000"/>
+<node TEXT="Term 1" POSITION="bottom_or_right" ID="ID_1391220495" CREATED="1788333539409" MODIFIED="1788340707667">
+<icon BUILTIN="button_ok"/>
+<font SIZE="16"/>
+<node TEXT="Emerging technology in Education" POSITION="bottom_or_right" ID="ID_322019608" CREATED="1788333601326" MODIFIED="1788341325683">
+<icon BUILTIN="button_ok"/>
+<font SIZE="16"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<node TEXT="Class link" POSITION="bottom_or_right" ID="ID_939182859" CREATED="1788334056933" MODIFIED="1788340269149" LINK="https://zoom.us/j/96410332038?pwd=lDpi7WNQAPr2Jf8IEDc0gdkJWVc8pH.1">
+<font SIZE="16"/>
+</node>
+<node TEXT="Assignments" POSITION="bottom_or_right" ID="ID_329724896" CREATED="1788334058445" MODIFIED="1788340269149">
+<font SIZE="16"/>
+<node TEXT="" ID="ID_284081536" CREATED="1788336406483" MODIFIED="1788336406483">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Assignment 1: Reflective log" ID="ID_269642967" CREATED="1788335157578" MODIFIED="1788340340828">
+<font SIZE="16"/>
+<node TEXT="Date of the assignment submission: 31 August, 2026" ID="ID_695405220" CREATED="1788335202683" MODIFIED="1788340372216">
+<font SIZE="16"/>
+</node>
+<node TEXT="Scope of the assignment: To write about the new learning and key takeaways from each class" ID="ID_1836680907" CREATED="1788335234858" MODIFIED="1788340372216">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="" ID="ID_1087815679" CREATED="1788336406472" MODIFIED="1788336406483">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="The purpose of this log was to document our learning. It was like a journal at the end of the day based on your learning" ID="ID_911131197" CREATED="1788336406490" MODIFIED="1788340379071">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="Assignment 2: Renewable assessment" ID="ID_1023047726" CREATED="1788335171477" MODIFIED="1788340340828">
+<font SIZE="16"/>
+<node TEXT="Date of assignment Submission: 31 August, 2026" ID="ID_1019188992" CREATED="1788335280862" MODIFIED="1788340340854">
+<font SIZE="16"/>
+</node>
+<node TEXT="Scope of the assignment: To use an emerging technology to make an assessment which would act as assessment as learning" ID="ID_1097029659" CREATED="1788335281219" MODIFIED="1788340340844">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="Assignment 3: Peer Assessment" ID="ID_500909601" CREATED="1788335181709" MODIFIED="1788340340828">
+<font SIZE="16"/>
+<node TEXT="Date of the assignment submission: 31 August, 2026" ID="ID_1388368042" CREATED="1788335202683" MODIFIED="1788340340828">
+<font SIZE="16"/>
+</node>
+<node TEXT="Scope of the assignment: Review an emerging technology- its pros, cons, etc." ID="ID_1876406537" CREATED="1788335620231" MODIFIED="1788340354840">
+<font SIZE="16"/>
+</node>
+</node>
+</node>
+<node TEXT="Resources" POSITION="bottom_or_right" ID="ID_1045860495" CREATED="1788334058940" MODIFIED="1788340269149" LINK="TISS%20Resources/ETE/">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="Learners and Learning" POSITION="bottom_or_right" ID="ID_359399224" CREATED="1788333602385" MODIFIED="1788340269167">
+<font SIZE="16"/>
+<node TEXT="Class link" ID="ID_1994591078" CREATED="1788334056933" MODIFIED="1788340269149" LINK="https://zoom.us/meeting/register/awkqUTreRXiZym8iA92YsQ">
+<font SIZE="16"/>
+</node>
+<node TEXT="Assignments" ID="ID_522768005" CREATED="1788334058445" MODIFIED="1788340269149">
+<font SIZE="16"/>
+</node>
+<node TEXT="Resources" ID="ID_1323706181" CREATED="1788334058940" MODIFIED="1788340269149">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="Curriculum, School and Teaching" POSITION="bottom_or_right" ID="ID_179191377" CREATED="1788333600686" MODIFIED="1788341193027">
+<icon BUILTIN="button_ok"/>
+<font SIZE="16"/>
+<node TEXT="Class link" ID="ID_1655536308" CREATED="1788334056933" MODIFIED="1788340269149" LINK="https://zoom.us/j/95990758688?pwd=bui9tJ6Tbg4e4PxZVRKuybgvF3cPeu.1">
+<font SIZE="16"/>
+</node>
+<node TEXT="Assignments" ID="ID_547748294" CREATED="1788334058445" MODIFIED="1788340269165">
+<font SIZE="16"/>
+</node>
+<node TEXT="Resources" ID="ID_826400083" CREATED="1788334058940" MODIFIED="1788340294616" LINK="TISS%20Resources/CST/">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="Philosophy" POSITION="bottom_or_right" ID="ID_226198171" CREATED="1788333598569" MODIFIED="1788340294624">
+<font SIZE="16"/>
+<node TEXT="Class link" ID="ID_1092782063" CREATED="1788334056933" MODIFIED="1788340294624" LINK="https://zoom.us/meeting/register/tojwPxxeRi-f7Nge7x79HA">
+<font SIZE="16"/>
+</node>
+<node TEXT="Assignments" ID="ID_664180053" CREATED="1788334058445" MODIFIED="1788340294624">
+<font SIZE="16"/>
+</node>
+<node TEXT="Resources" ID="ID_1492954609" CREATED="1788334058940" MODIFIED="1788340294626">
+<font SIZE="16"/>
+</node>
+</node>
+</node>
+<node TEXT="Term 2" POSITION="bottom_or_right" ID="ID_631497382" CREATED="1788333536023" MODIFIED="1788340300188" HGAP_QUANTITY="21.8 pt" VSHIFT_QUANTITY="53.4 pt">
+<font SIZE="16"/>
+<node TEXT="Learning Design" ID="ID_1140903452" CREATED="1788333797149" MODIFIED="1788340294626">
+<font SIZE="16"/>
+<node TEXT="Class link" ID="ID_1318879824" CREATED="1788334056933" MODIFIED="1788340294626" LINK="https://zoom.us/j/93503139948?pwd=0itzJUhnAu7Mq4OFmVnhTOh3hGRvqi.1">
+<font SIZE="16"/>
+</node>
+<node TEXT="Assignments" ID="ID_1465578107" CREATED="1788334058445" MODIFIED="1788340306417">
+<font SIZE="16"/>
+</node>
+<node TEXT="Resources" ID="ID_159812068" CREATED="1788334058940" MODIFIED="1788340306427">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="Product Management" ID="ID_1199036679" CREATED="1788333834296" MODIFIED="1788340294626">
+<font SIZE="16"/>
+<node TEXT="Class link" ID="ID_1657715135" CREATED="1788334056933" MODIFIED="1788340294628" LINK="https://zoom.us/j/93605690854?pwd=DLbwCt77RelH95ByRhpqXfDpoCgBay.1">
+<font SIZE="16"/>
+</node>
+<node TEXT="Assignments" ID="ID_1662323129" CREATED="1788334058445" MODIFIED="1788340294628">
+<font SIZE="16"/>
+</node>
+<node TEXT="Resources" ID="ID_1321190054" CREATED="1788334058940" MODIFIED="1788340294628">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="Reading, writing and thinking" ID="ID_1393953976" CREATED="1788333840791" MODIFIED="1788340294628">
+<font SIZE="16"/>
+<node TEXT="Class link" ID="ID_1724182799" CREATED="1788334056933" MODIFIED="1788340294628" LINK="https://zoom.us/j/92823032055?pwd=rbsbKayHSTyjVN9sPUS1fSX8GinYsO.1">
+<font SIZE="16"/>
+</node>
+<node TEXT="Assignments" ID="ID_1872911593" CREATED="1788334058445" MODIFIED="1788340311288">
+<font SIZE="16"/>
+</node>
+<node TEXT="Resources" ID="ID_630982080" CREATED="1788334058940" MODIFIED="1788340311297">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="Seminar" ID="ID_420042718" CREATED="1788333991705" MODIFIED="1788340294628">
+<font SIZE="16"/>
+<node TEXT="Class link" ID="ID_1216635976" CREATED="1788334056933" MODIFIED="1788340317014">
+<font SIZE="16"/>
+</node>
+<node TEXT="Assignments" ID="ID_1441315655" CREATED="1788334058445" MODIFIED="1788340317016">
+<font SIZE="16"/>
+</node>
+<node TEXT="Resources" ID="ID_1681278066" CREATED="1788334058940" MODIFIED="1788340317024">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="Capstone" ID="ID_179963611" CREATED="1788333860266" MODIFIED="1788340294628">
+<font SIZE="16"/>
+<node TEXT="Class link" ID="ID_1329065614" CREATED="1788334056933" MODIFIED="1788340294628">
+<font SIZE="16"/>
+</node>
+<node TEXT="Assignments" ID="ID_565398618" CREATED="1788334058445" MODIFIED="1788340294632">
+<font SIZE="16"/>
+</node>
+<node TEXT="Resources" ID="ID_1957221133" CREATED="1788334058940" MODIFIED="1788340294632">
+<font SIZE="16"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Sem 2" FOLDED="true" POSITION="top_or_left" ID="ID_1344437667" CREATED="1788333440214" MODIFIED="1788340677301">
+<edge COLOR="#0000ff"/>
+<font SIZE="16"/>
+<node TEXT="Term 3" POSITION="top_or_left" ID="ID_769669119" CREATED="1788333564975" MODIFIED="1788333572852"/>
+<node TEXT="Term 4" POSITION="top_or_left" ID="ID_1274873609" CREATED="1788333562392" MODIFIED="1788333580900"/>
+</node>
+</node>
+</map>
